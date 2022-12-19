2eb34732147c81203d4999fc157961b24edfd46f
JKJIN1999 <jugyeongkim911@gmail.com>
2022-12-19 10:11:24 +0900

    space

--- src/gecnk/main.py
+++ src/gecnk/main.py
@@ -18,6 +18,7 @@ result_file = "./src"
 def main(data_directory, error_list, result_directory, json_maximum, tokenizer_type, label_type, error_by):
     noise(data_directory, error_list,
           result_directory, json_maximum, tokenizer_type, label_type, error_by)
+    
     for x in error_list:
         error_name += "_" + x
     zip_name = (data_directory.split("/")[-1] + error_name)
